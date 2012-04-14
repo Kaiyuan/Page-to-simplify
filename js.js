8a8a92415149f06607d615d6672e010db048939d
Kaiyuan <sealour@hotmail.com>
2012-04-14 08:01:44 +0800

    delete js

--- js.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load",function(){
-var head = document.getElementsByTagName('HEAD').item(0);
-var style = document.createElement('link');
-style.href = 'css/style.css';
-style.rel = 'stylesheet';
-style.type = 'text/css';
-head.appendChild(style);
-});
